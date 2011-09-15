cf2e7e5ec9a6c1fdc6627cc8cf0f35e0d392c8f3
Simon Walker <s.r.walker101@googlemail.com>
2011-09-15 12:05:25 +0100

    running tests

--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,10 @@
 #include "FitsObject.h"
+#include <UnitTest++/UnitTest++.h>
 
 using namespace std;
 
 int main(int argc, const char *argv[])
 {
     
-    return 0;
+    return UnitTest::RunAllTests();
 }
