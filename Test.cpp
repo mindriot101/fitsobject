5b8b9fd3b5f523fa56e1e045e13bb9bd76ef36a6
Simon Walker <s.r.walker101@googlemail.com>
2011-09-18 11:25:20 +0100

    added tests

--- Test.cpp
+++ Test.cpp
@@ -74,6 +74,19 @@ TEST_FIXTURE(TableFixture, ColnumTest)
     CHECK_EQUAL(2, f->columnNumber("V_mag"));
 }
 
+TEST_FIXTURE(TableFixture, StringColumnRetrievalTest)
+{
+    vector<string> data = f->columnData<string>("target");
+    string Target("NGC1001");
+    CHECK_EQUAL(Target, data[0]);
+}
+
+TEST_FIXTURE(TableFixture, DoubleColumnRetrievalTest)
+{
+    vector<double> data = f->columnData<double>("V_mag");
+    CHECK_CLOSE(11.1, data[0], 0.05);
+}
+
 int main(int argc, const char *argv[])
 {
     return UnitTest::RunAllTests();
