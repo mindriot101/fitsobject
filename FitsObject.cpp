a48ae03b873c10b30ab952a4b19eaf27f244d636
Simon Walker <s.r.walker101@googlemail.com>
2012-01-17 10:36:54 +0000

    stopping deprecated string constant warning

--- FitsObject.cpp
+++ FitsObject.cpp
@@ -210,10 +210,11 @@ vector<string> Fits::columnData(const string &columnname)
 
 
     vector<string> Objects;
+    string nullstr = "";
     for (int i=0; i<nrows; ++i)
     {
         char Name[dispwidth+1], *nptr=(char*)Name;
-        fits_read_col_str(*this->fptr(), colno, i+1, 1, 1, (char*)"", &nptr, NULL, &this->status());
+        fits_read_col_str(*this->fptr(), colno, i+1, 1, 1, const_cast<char*>(nullstr.c_str()), &nptr, NULL, &this->status());
         this->check();
 
         Objects.push_back(Name);
